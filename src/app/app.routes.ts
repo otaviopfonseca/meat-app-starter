a9182166ca1705921a64db4a6f385461ebc7b8b6
Otávio Fonseca <otavio.fonseca@dtidigital.com.br>
2018-11-22 11:34:24 -0200

    Implementação do canLoad e canActivate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,9 +7,12 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './security/login/login.component';
+import { LoggedInGuard } from './security/loggedin.guard';
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'login/:to', component: LoginComponent },
+    { path: 'login', component: LoginComponent },
     { path: 'about', loadChildren: './about/about.module.ts#AboutModule' },
     { path: 'restaurants', component: RestaurantsComponent },
     { path: 'restaurants/:id', component: RestaurantDetailComponent,
@@ -19,8 +22,7 @@ export const ROUTES: Routes = [
             { path: 'reviews', component: ReviewsComponent }
         ]
     },
-    { path: 'order', loadChildren: './order/order.module#OrderModule' },
+    { path: 'order', loadChildren: './order/order.module#OrderModule', canLoad: [LoggedInGuard], canActivate: [LoggedInGuard]},
     { path: 'order-summary', component: OrderSummaryComponent },
-    { path: 'login', component: LoginComponent },
     { path: '**', component: NotFoundComponent },
 ];
--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginService } from './login.service';
 import { NotificationService } from '../../shared/messages/notification.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'mt-login',
@@ -11,22 +12,29 @@ import { NotificationService } from '../../shared/messages/notification.service'
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
+  navigateTo: string;
 
   constructor(private formBuilder: FormBuilder,
     private loginService: LoginService,
-    private notificationService: NotificationService) { }
+    private notificationService: NotificationService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
       email: this.formBuilder.control('', [Validators.required, Validators.email]),
       password: this.formBuilder.control('', [Validators.required])
-    })
+    });
+    this.navigateTo = this.activatedRoute.snapshot.params['to'] || btoa('/');
   }
 
   login() {
     this.loginService.login(this.loginForm.value.email,
                             this.loginForm.value.password)
                       .subscribe(user => this.notificationService.notify(`Bem Vindo, ${user.name}`),
-                        errorResponse => this.notificationService.notify(errorResponse.error.message));
+                        errorResponse => this.notificationService.notify(errorResponse.error.message),
+                        () => {
+                          this.router.navigate([atob(this.navigateTo)])
+                        });
   }
 }
