6fdbcf4690aec905b6dca9a4d030bc98b4316a7e
Otávio Fonseca <otavio.fonseca@dtidigital.com.br>
2019-04-18 18:11:58 -0300

    Correções sonar

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -6,8 +6,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'mt-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
 
